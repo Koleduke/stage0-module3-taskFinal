089b39c55b67c1554d2766c52bd9cc76b9e6a127
Koleduke <ydel2014@gmail.com>
2023-11-22 23:32:20 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        String s = String.valueOf(number);
+
+        int f1 = Integer.parseInt(String.valueOf(s.charAt(0)));
+        int f2 = Integer.parseInt(String.valueOf(s.charAt(1)));
+        int f3 = Integer.parseInt(String.valueOf(s.charAt(2)));
+        int f4 = Integer.parseInt(String.valueOf(s.charAt(3)));
+        int res = f1+ f2+f3+f4;
+        System.out.println(res);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String s = String.valueOf(number);
+        String r= String.valueOf(s.charAt(0));
+        String e = String.valueOf(s.charAt(1));
+        String z = String.valueOf(s.charAt(2));
+        String res = z+e + r;
+        System.out.println(res);
     }
 }
